2fc3144cd4b670bdb6f52c745f8b9d38b16e6112
xiaoqiang zhao <zxq_yx_007@163.com>
2017-05-12 10:37:05 +0800

    update to latest algorithm (arm)

--- static_kallsyms.py
+++ static_kallsyms.py
@@ -4,7 +4,10 @@ import sys
 import struct
 
 #The default address at which the kernel text segment is loaded
-DEFAULT_KERNEL_TEXT_START = 0xC0008000
+DEFAULT_KERNEL_TEXT_START = 0xc0008000
+
+# kernel address start
+KERNEL_START = 0xc0000000
 
 #The size of the DWORD in a 32-bit architecture
 DWORD_SIZE = struct.calcsize("I")
@@ -13,7 +16,7 @@ DWORD_SIZE = struct.calcsize("I")
 WORD_SIZE = struct.calcsize("H")
 
 #The alignment of labels in the resulting kernel file
-LABEL_ALIGN = DWORD_SIZE * 4
+LABEL_ALIGN = 4
 
 #The minimal number of repeating addresses pointing to the kernel's text start address
 #which are used as a heuristic in order to find the beginning of the kernel's symbol
@@ -54,53 +57,133 @@ def label_align(address):
 	'''
 	Aligns the given value to the closest label output boundry
 	'''
-	return address & ~(LABEL_ALIGN-1)
+	return ((address + LABEL_ALIGN - 1) / LABEL_ALIGN) * LABEL_ALIGN
 
 def find_kallsyms_addresses(kernel_data, kernel_text_start):
 	'''
 	Searching for the beginning of the kernel's symbol table
 	Returns the offset of the kernel's symbol table, or -1 if the symbol table could not be found
 	'''
-        search_str = struct.pack("<4I", DEFAULT_KERNEL_TEXT_START, DEFAULT_KERNEL_TEXT_START, DEFAULT_KERNEL_TEXT_START + 0x4c, DEFAULT_KERNEL_TEXT_START + 0x100 )
-	#search_str = struct.pack("<I", DEFAULT_KERNEL_TEXT_START) * KALLSYMS_ADDRESSES_MIN_HEURISTIC
-	return kernel_data.find(search_str)
+        '''
+        search_str = struct.pack("<4Q", DEFAULT_KERNEL_TEXT_START, DEFAULT_KERNEL_TEXT_START + 0x40
+                                 , DEFAULT_KERNEL_TEXT_START + 0x80, DEFAULT_KERNEL_TEXT_START + 0x80 )
+        offset =  kernel_data.find(search_str)
+        '''
+        # based on symbol number count
 
-def get_kernel_symbol_table(kernel_data, kernel_text_start):	
-	'''
+        count = 0
+        offset = 0
+        offset2 = 0
+        find = False
+
+        while offset < len(kernel_data):
+            value = read_dword(kernel_data, offset)
+
+            offset2 = offset
+            while value >= KERNEL_START:
+                count += 1
+                offset2 += DWORD_SIZE
+                value = read_dword(kernel_data, offset2)
+
+                if count > 20000:
+                    find = True
+                    break
+
+            if find:
+                break
+            else:
+                count = 0
+                offset = offset2 + DWORD_SIZE
+
+        return offset
+
+def get_kernel_symbol_table(kernel_data, kernel_text_start):
+        '''
 	Retrieves the kernel's symbol table from the given kernel file
 	'''
 
-	#Getting the beginning and end of the kallsyms_addresses table
-	kallsyms_addresses_off = find_kallsyms_addresses(kernel_data, kernel_text_start)
+        #Getting the beginning and end of the kallsyms_addresses table
+        kallsyms_addresses_off = find_kallsyms_addresses(kernel_data, kernel_text_start)
         print "kernel symbol table offset: %d" % kallsyms_addresses_off
         if kallsyms_addresses_off == -1:
             return None
 
 	kallsyms_addresses_end_off = kernel_data.find(struct.pack("<I", 0), kallsyms_addresses_off)
+        #print "kallsyms_address_end_off = %d" % kallsyms_addresses_end_off
 	num_symbols = (kallsyms_addresses_end_off - kallsyms_addresses_off) / DWORD_SIZE
         print "symbol number: %d" % num_symbols
 
 	#Making sure that kallsyms_num_syms matches the table size
-	kallsyms_num_syms_off = label_align(kallsyms_addresses_end_off + LABEL_ALIGN)
-	kallsyms_num_syms = read_dword(kernel_data, kallsyms_num_syms_off)
+	kallsyms_num_syms_off = kallsyms_addresses_end_off
+        #print "kallsyms_num_syms_off = %d" % kallsyms_num_syms_off
+
+        # skip 0UL
+        kallsyms_num_syms = read_dword(kernel_data, kallsyms_num_syms_off)
+        while kallsyms_num_syms == 0:
+            kallsyms_num_syms_off += DWORD_SIZE
+            kallsyms_num_syms = read_dword(kernel_data, kallsyms_num_syms_off)
+            #print "kallsyms_num_syms = %d" % kallsyms_num_syms
+
 	if kallsyms_num_syms != num_symbols:
 		print "[-] Actual symbol table size: %d, read symbol table size: %d" % (num_symbols, kallsyms_num_syms)
 		return None
 
 	#Calculating the location of the markers table
-	kallsyms_names_off = label_align(kallsyms_num_syms_off + LABEL_ALIGN)
+	kallsyms_names_off = kallsyms_num_syms_off + DWORD_SIZE
+
+        # skip 0UL
+        value = read_dword(kernel_data, kallsyms_names_off)
+        while value == 0:
+            kallsyms_names_off += DWORD_SIZE
+            value = read_dword(kernel_data, kallsyms_names_off)
+            #print "value1 = %d" % value
+
 	current_offset = kallsyms_names_off
 	for i in range(0, num_symbols):
 		current_offset += read_byte(kernel_data, current_offset) + 1
-	kallsyms_markers_off = label_align(current_offset + LABEL_ALIGN)
+
+	kallsyms_markers_off = label_align(current_offset)
+
+        # skip 0UL
+        value = read_dword(kernel_data, kallsyms_markers_off)
+        while value == 0:
+            kallsyms_markers_off += DWORD_SIZE
+            value = read_dword(kernel_data, kallsyms_markers_off)
+            #print "value2 = %d" % value
+
+        # start with 0x0
+        kallsyms_markers_off -= DWORD_SIZE
+
+        mark_num = (num_symbols / 256 + 1) if num_symbols % 256 != 0  else num_symbols / 256
+	kallsyms_token_table_off = kallsyms_markers_off + mark_num * DWORD_SIZE
+
+        # skip 0UL
+        value = read_dword(kernel_data, kallsyms_token_table_off)
+        while value == 0:
+            kallsyms_token_table_off += DWORD_SIZE
+            value = read_dword(kernel_data, kallsyms_token_table_off)
+            #print "value3 = %d" % value
 
 	#Reading the token table
-	kallsyms_token_table_off = label_align(kallsyms_markers_off + (((num_symbols + 255) >> 8) * DWORD_SIZE))
 	current_offset = kallsyms_token_table_off
 	for i in range(0, 256):
 		token_str = read_c_string(kernel_data, current_offset)
+                #print "str = %s" % token_str
 		current_offset += len(token_str) + 1
-	kallsyms_token_index_off = label_align(current_offset + LABEL_ALIGN)
+
+	kallsyms_token_index_off = label_align(current_offset)
+
+        #print "index_off = %x" % kallsyms_token_index_off
+
+        # skip 0x00
+        value = read_word(kernel_data, kallsyms_token_index_off)
+        while value == 0:
+            kallsyms_token_index_off += WORD_SIZE
+            value = read_word(kernel_data, kallsyms_token_index_off)
+            #print "value4 = %d" %value
+
+        # start with 0x00
+        kallsyms_token_index_off -= WORD_SIZE
 
 	#Creating the token table
 	token_table = []
@@ -122,7 +205,7 @@ def get_kernel_symbol_table(kernel_data, kernel_text_start):
 
 		symbol_address = read_dword(kernel_data, kallsyms_addresses_off + i * DWORD_SIZE)
 		symbol_table.append((symbol_address, symbol_name[0], symbol_name[1:]))
-		
+
 	return symbol_table
 
 def main():
@@ -146,7 +229,7 @@ def main():
             print "Done!"
         else:
             print "can not find symbol table, quit!"
-	
+
 
 if __name__ == "__main__":
 	main()
